6fea54d9bf3398d2b5388c25761d2e6e66a2ae07
Karthick <karthickcool17@gmail.com>
2018-09-07 16:21:22 +0530

    Queue name prefix added in application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Forum
 
     # Use a real queuing backend for Active Job (and separate queues per environment)
     config.active_job.queue_adapter     = :resque
-   # config.active_job.queue_name_prefix = "promptcloud_forum_#{Rails.env}"
+    config.active_job.queue_name_prefix = "ror_prod_thredded"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
